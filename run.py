1bc98e26d592523aa4f49678cec8d7e54b8c6136
caketaster <caketaster@yahoo.co.uk>
2023-09-12 08:44:16 +0000

    Add data to Flask unit, iterating with For loops

--- run.py
+++ run.py
@@ -10,19 +10,19 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+@app.route("/about", page_title="About")
 def about() :
-    return render_template("about.html")
+    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
 def contact() :
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers() :
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
